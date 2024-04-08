fd8b7495922586278032d8f65dc7c78c183ed735
Chad Boyce <chadboyce@gmail.com>
2024-04-08 14:12:43 +0000

    added lazyvim install

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -5,3 +5,8 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.
 
 tar -xzf nvim-linux64.tar.gz
 cp -r nvim-linux64/* /usr
+
+echo "installed Neovim"
+
+git clone https://github.com/lazyvim/starter $HOME/.config/nvim
+echo "installed LazyVim"
